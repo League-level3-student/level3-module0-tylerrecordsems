e951f7598637aa75f5619131d024daecc2b4667e
root <root@68ba5067c5e3>
2021-12-14 02:20:06 +0000

    yay ;(

--- src/_00_Intro_To_Arrays/_02_RobotRace.java
+++ src/_00_Intro_To_Arrays/_02_RobotRace.java
@@ -8,11 +8,13 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _02_RobotRace {
     // 1. make a main method
-
+public static void main(String[] args) {
+	
+}
         // 2. create an array of 5 robots.
-
+Robot[] robots = new Robot[5];
         // 3. use a for loop to initialize the robots.
-
+int[] ints = {0,1,2,3,4};
         // 4. make each robot start at the bottom of the screen, side by side, facing up
     
         // 5. use another for loop to iterate through the array and make each robot move
--- src/_00_Intro_To_Arrays/_03_FindTheHiddenButton.java
+++ src/_00_Intro_To_Arrays/_03_FindTheHiddenButton.java
@@ -16,9 +16,9 @@ public class _03_FindTheHiddenButton implements ActionListener{
     JPanel panel;
 
     // 1. create an array of JButtons. Don't initialize it yet.
-
+JButton[] Button = new JButton[6];
     // 2. create an integer variable called hiddenButton
-
+int hiddenButton;
     public static void main(String[] args) {
         new _03_FindTheHiddenButton().start();
     }
@@ -29,33 +29,37 @@ public class _03_FindTheHiddenButton implements ActionListener{
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // 3. Ask the user to enter a positive number and convert it to an int
-
+String number = JOptionPane.showInputDialog("Enter a positive number.");
+int posnumber=Integer.parseInt(number);
         // 4. Initialize the array of JButtons to be the size of the int
         //    created in step 3
-
+JButton[] Button = new JButton[posnumber];
         // 5. Make a for loop to iterate through the JButton array
-        
+        for (int i = 0; i < posnumber; i++) {
             // 6. initialize each JButton in the array
-        
+      Button[i] = new JButton();
             // 7. add the ActionListener to each JButton
-        
+        Button[i].addActionListener(this);
             // 8. add each JButton to the panel
-
+        	panel.add(Button[i]);
+        }
         // 9 add the panel to the window
-
+window.add(panel);
         // 10. call setExtendedState(JFrame.MAXIMIZED_BOTH) on your JFrame object.
-
+window.setExtendedState(JFrame.MAXIMIZED_BOTH);
         // 11. set the JFrame to visible.
-
+window.setVisible(true);
         // 12. Give the user the instructions for the game.
 
         // 13. initialize the hiddenButton variable to a random number less than
         //     the int created in step 3
 
+Random ran= new Random(posnumber-1);
+//int hiddenButton = ran;
         // 14. Set the text of the JButton located at hiddenButton to read "ME"
 
         // 15. Use Thread.sleep(100); to pause the program.
-
+//Thread.sleep(100);
         // 16. Set the text of the JButton located at hiddenButton to be blank.
 
     }
