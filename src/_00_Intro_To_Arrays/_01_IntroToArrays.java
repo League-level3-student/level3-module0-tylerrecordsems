ba03d289e862bce5eaaaaf3bd1bf7ce33a8dd8f4
student <student@ip-172-31-61-153.us-west-2.compute.internal>
2022-01-31 18:26:37 -0800

    arrays are fun

--- src/_00_Intro_To_Arrays/_01_IntroToArrays.java
+++ src/_00_Intro_To_Arrays/_01_IntroToArrays.java
@@ -5,24 +5,37 @@ import java.util.Random;
 public class _01_IntroToArrays {
     public static void main(String[] args) {
         // 1. declare and Initialize an array 5 Strings
-
+String yeah[]= new String[5];
+yeah[0]="q";
+yeah[1]="w";
+yeah[2]="e";
+yeah[3]="r";
+yeah[4]="t";
         // 2. print the third element in the array
-
+System.out.println(yeah[2]);
         // 3. set the third element to a different value
-
+yeah[2]="ee";
         // 4. print the third element again
-
+System.out.println(yeah[2]);
         // 5. use a for loop to set all the elements in the array to a string
         //    of your choice
 
         // 6. use a for loop to print all the values in the array
         //    BE SURE TO USE THE ARRAY'S length VARIABLE
-
+for (int i = 0; i < 5; i++) {
+	System.out.println(yeah[i]);
+}
         // 7. make an array of 50 integers
+for (int i = 0; i < 50; i++) {
+int[] bro = {i};	
+}
 
         // 8. use a for loop to make every value of the integer array a random
         //    number
-
+for (int i = 0; i < 50; i++) {
+	Random ran = new Random(50);
+	/// START : bro[i]=ran();
+}
         // 9. without printing the entire array, print only the smallest number
         //    on the array
 
---
+++ src/um.java
@@ -0,0 +1,28 @@
+
+
+public class um {
+	
+	public static boolean yes() {
+		boolean arrived = yes();
+		
+		if(arrived) {
+			return true;
+		}
+		else {
+			return yes();
+		}
+	}
+public static void main(String[] args) {
+		
+	
+		// Put a breakpoint here
+		
+		int thisiszero = 0;
+		
+		while (thisiszero == 0) {
+//			System.out.println("It is still zero");
+		}
+		
+		System.out.println("I'm freeeeee!");
+	}
+}
